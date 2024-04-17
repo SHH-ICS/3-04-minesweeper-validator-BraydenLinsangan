868f0291ff4253033732f9f85bd7a2a3bb8b5958
BraydenLinsangan <159149939+BraydenLinsangan@users.noreply.github.com>
2024-04-17 13:26:56 +0000

    COOL

--- sweep.py
+++ sweep.py
@@ -1,13 +1,30 @@
+import time
 # Validate a minesweeper interior block
 # block_data is a two dimensional array containing the data from a 3 x 3 grid of squares
 # We are assuming that we are only checking interior blocks for now
 # Return value should be a string that says either Valid or Invalid (with some hints as to why it's invlaid)
-def validate( block_data ):
   # Check whether the centre block is a bomb, a number, or an invalid input
   # Skip bombs, send an error on invalid input, verify numbers
-
-  return
-
+def validate( block_data ):
+  bomb_count = 0
+  for i in range (3):
+    for j in range (3):
+      if i != 1 and j != 1:
+        if block_data[i][j] == -1:
+          bomb_count = bomb_count + 1
+          if block_data[1][1] == -1:
+            def myfunction():
+              return "bomb"
+            print(myfunction())
+          if block_data[1][1] >= 1:
+            def myfunction():
+              return "valid"
+            print(myfunction())
+          if block_data[1][1] <= -2:
+            def myfunction():
+              return "invalid"
+            print(myfunction())
+            return(block_data)
 
 grid = [
   [-1,1,0],
